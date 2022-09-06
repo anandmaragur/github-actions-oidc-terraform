f4b5e4f7c89b2bf01f55da130660ee5e9076b77f
Anand Maragur <anand.maragur@gmail.com>
2022-09-06 12:30:28 +0800

    EKS CLuster

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,9 @@
-variable "resource_group_location" {
-  default     = "southcentralus"
-  description = "Location of the resource group."
+variable "appId" {
+  description = "Azure Kubernetes Service Cluster service principal"
+  default = "3d4dd55a-5f06-44b4-bfbd-247e3a9081e9"
 }
 
-variable "resource_group_name" {
-  default = "cd-teroidc"
-  description = "Name of the resource group."
+variable "password" {
+  description = "Azure Kubernetes Service Cluster password"
+  default = "NgO8Q~XSDZVSAyy4jnTmvSkvoTdqb9.Ph1Aloaer"
 }
